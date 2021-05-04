7f8f39269eae87b413770d4e78c7ab7d52933d57
Baptiste Harle <baptiste.harle@gmail.com>
2021-05-04 17:54:11 +0200

    add backend

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+
+const cameraRoutes = require('./routes/camera');
+const teddyRoutes = require('./routes/teddy');
+const furnitureRoutes = require('./routes/furniture');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// definition des routes front 
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/product/:uuid', express.static(path.join(__dirname, 'public/html/product.html')));
+app.use('/order', express.static(path.join(__dirname, 'public/html/order.html')));
+app.use('/congrats', express.static(path.join(__dirname, 'public/html/order-congrats.html')));
+
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static('images'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use('/api/cameras', cameraRoutes);
+app.use('/api/teddies', teddyRoutes);
+app.use('/api/furniture', furnitureRoutes);
+
+
+module.exports = app;
